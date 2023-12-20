423dd72963707acf9f728034d197cd6f0d75aa14
pascualsolutions <alexpirot83@gmail.com>
2023-12-20 13:29:53 +0100

    Add page and edit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return inertia('Posts/Index', [
+            'posts' => Post::all(),
+        ]);
+    }
+
+
+    public function create()
+    {
+       //
+    }
 }
+
